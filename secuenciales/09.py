696a70dd18c31b17f9a8cfccd1379e813097251e
EiderMontalvo <sanchezmontalvoeider@gmail.com>
2024-10-10 11:43:29 -0500

    tareaPY

---
+++ secuenciales/09.py
@@ -0,0 +1,9 @@
+num = int(input("ingresa un número entero de 4 cifras: "))
+
+if 1000 <= num <= 9999:
+    suma = 0
+    for cifra in str(num):
+        suma += int(cifra)
+    print("La suma de las cifras es:", suma)
+else:
+    print("ingresar un número de 4 cifras.")
